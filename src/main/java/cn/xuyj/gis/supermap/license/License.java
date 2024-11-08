5a99a503e2ffc1386087323e6a4de9511e04a309
徐永杰 <xuyj@dist.com.cn>
2024-11-08 11:08:12 +0800

    feat:在线许可登录和登出

---
+++ src/main/java/cn/xuyj/gis/supermap/license/License.java
@@ -0,0 +1,69 @@
+package cn.xuyj.gis.supermap.license;
+
+import lombok.Data;
+import com.supermap.data.bslicense.BSLicense;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * @author xuyj
+ * @since 2024/11/8 10:22
+ */
+@Data
+public class License {
+    /**
+     * 在线许可服务器地址
+     */
+    private String ip;
+
+    /**
+     * 服务器端口号
+     * 默认端口号：9183
+     */
+    private int port = 9183;
+
+    /**
+     * 许可模块
+     * 默认是单模块：65400
+     */
+    private int[] modules = new int[]{65400};
+
+    /**
+     * 登录在线许可
+     */
+    public void login() {
+        System.out.println("在线许可验证");
+        System.out.println("许可ip:" + ip);
+        System.out.println("许可port:" + port);
+        System.out.print("许可modules:");
+        for (int i = 0; i < modules.length; i++) {
+            System.out.println(modules[i]);
+        }
+        try {
+            boolean login = BSLicense.login(ip, port, modules);
+            if (login)
+                System.out.println("验证成功！");
+            else
+                System.out.printf("验证失败");
+        } catch (Exception e) {
+            System.out.println("在线许可验证失败！！！");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 登出在线许可
+     */
+    public void logout() {
+        System.out.println("登出在线许可");
+        BSLicense.logout();
+    }
+
+    public static void main(String[] args) {
+        License license = new License();
+        license.setIp("192.168.200.60");
+        license.login();
+        license.logout();
+    }
+}
