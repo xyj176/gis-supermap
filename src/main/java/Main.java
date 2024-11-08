09347850329afcfd29f59afa24d6856e5db72821
徐永杰 <xuyj@dist.com.cn>
2024-11-08 10:00:09 +0800

    feat:验证iobject环境

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import com.supermap.data.Workspace;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("验证iObject环境");
+        Workspace workspace = new Workspace();
+        workspace.dispose();
         System.out.println("gis-supermap!");
     }
 }
